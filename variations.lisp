5bb8fdceb503c9dafb8781691b89599fa39c2d03
Giorgos Pontikakis <giorgos.pontikakis@gmail.com>
2012-06-18 23:37:54 +0300

    textbox now may become an anchor when given a href

--- variations.lisp
+++ variations.lisp
@@ -8,23 +8,31 @@
 ;;; ------------------------------------------------------------
 
 (defclass textbox (input-text)
-  ()
+  ((href :accessor href :initarg :href))
   (:default-initargs :disabled nil
                      :name (error 'slot-uninitialized :class 'textbox :slot 'name)
                      :value nil
                      :readonly nil
-                     :password nil))
+                     :password nil
+                     :href nil))
 
-(defmethod display ((textbox textbox) &key
-                    id css-class value name
-                    (readonly nil readonly-s) (disabled nil disabled-s) (password nil password-s))
+(defmethod display ((textbox textbox) &key id css-class value name href
+                                           (readonly nil readonly-s)
+                                           (disabled nil disabled-s)
+                                           (password nil password-s))
   (let ((disabled-p (if disabled-s disabled (disabled textbox)))
         (password-p (if password-s password (password textbox))))
     (if disabled-p
-        (with-html
-          (:span :id (or id (id textbox))
-                 :class (or css-class (css-class textbox))
-                 (str (lisp->html (or value (value textbox) :null)))))
+        (if (or href (href textbox))
+            (with-html
+              (:a :id (or id (id textbox))
+                  :class (or css-class (css-class textbox))
+                  :href (or href (href textbox))
+                  (str (lisp->html (or value (value textbox) :null)))))
+            (with-html
+              (:span :id (or id (id textbox))
+                     :class (or css-class (css-class textbox))
+                     (str (lisp->html (or value (value textbox) :null))))))
         (with-html
           (:input :id (or id (id textbox))
                   :class (or css-class (css-class textbox))
@@ -33,7 +41,7 @@
                   :value (lisp->html (or value (value textbox) :null))
                   :readonly (if readonly-s readonly (readonly textbox)))))))
 
-(defun textbox (name &key id css-class disabled value readonly password)
+(defun textbox (name &key id css-class disabled value readonly password href)
   (display (make-instance 'textbox
                           :id id
                           :css-class css-class
@@ -41,4 +49,5 @@
                           :name name
                           :value value
                           :readonly readonly
-                          :password password)))
+                          :password password
+                          :href href)))
